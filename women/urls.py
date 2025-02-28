d9de54c50c1d49a47a7851ffcd26e8e325615c98
Beslan <bes-zaurbaev@yandex.ru>
2025-02-28 11:15:56 +0300

    Dinamic URL

--- women/urls.py
+++ women/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, register_converter
 from women import views
+from women import converters
+
+register_converter(converters.FourDigitYearConverter, 'year4')
  
 urlpatterns = [
     path('', views.index),
-    path('cats/', views.categories),
+    path('cats/<int:ind_cat>/', views.categories),
+    path('cats/<str:ind_cat>/', views.categories),
+    path('arhive/<year4:year>/', views.arhive)
 ]
--- women/views.py
+++ women/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
+
 def index(request):
     return HttpResponse("Главная страница сайта!")
 
 
-def categories(request):
-    return HttpResponse("Страница категории 2")
+def categories(request, ind_cat):
+    return HttpResponse(f"Страница категории {ind_cat}")
+
+def arhive(request, year):
+    return HttpResponse(f"Архив за {year} год")
